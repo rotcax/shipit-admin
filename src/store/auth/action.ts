4a39e694a286833ff52088e203a264f656825ccb
Victor Caceres <caceres.vc95@gmail.com>
2021-04-10 10:41:55 -0400

    Logic login action

--- src/store/auth/action.ts
+++ src/store/auth/action.ts
@@ -1,7 +1,19 @@
 import { actionObject } from '@utils'
 import { CredentialsProps } from '@types'
+import { users } from '@repository'
 import { LOGIN } from './action-types'
 
 export const login = (credentials: CredentialsProps) => {
-  return actionObject(LOGIN, credentials)
+  try {
+    const { email, password } = credentials
+    const user = users.find(user => user.email == email)
+
+    if(!user) throw 'Invalid user'
+    if(user.password != password) throw 'Invalid password'
+
+    return actionObject(LOGIN, { email: user.email, isAuth: true })
+
+  } catch(e) {
+    console.log(e);
+  }
 }
