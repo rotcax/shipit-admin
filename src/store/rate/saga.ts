6a425c703b3bb52268856aeba505d5adac5e227e
Victor Caceres <caceres.vc95@gmail.com>
2021-04-11 15:32:24 -0400

    Set shipments value if a rate is made

--- src/store/rate/saga.ts
+++ src/store/rate/saga.ts
@@ -3,20 +3,40 @@ import { AnyAction } from 'redux'
 import { actionObject, fetchService } from '@utils'
 import { rates } from '@utils/path'
 import { CREATE_RATE, CREATE_RATE_ASYNC } from './action-types'
-import { getAuth } from '../selectors'
+import { SET_SHIPMENT_VALUES } from '../shipment/action-types'
+import { getAuth, getCourier } from '../selectors'
 
 export function* createRateAsync({ payload }: AnyAction) {
   try {
     const { email, accessToken } = yield select(getAuth)
     const parcel = {
       ...payload,
-      'type_of_destiny': 'domicilio',
-      'algorithm': '1',
-      'algorithm_days': '1'
+      type_of_destiny: 'domicilio',
+      algorithm: '1',
+      algorithm_days: '1'
     }
 
 		const response = yield call(fetchService, rates, 'POST', { parcel }, { email, accessToken })
     yield put(actionObject(CREATE_RATE_ASYNC, { betterPrice: response }))
+
+    const { length, width, height, weight, destiny_id } = payload
+    const { lower_price: { courier: { name } } } = response
+    const { couriers } = yield select(getCourier)
+
+    const sizes = { length, width, height, weight }
+    const destiny = { commune_id: destiny_id }
+
+    const currentCourier = couriers.find(courier => courier.name.toLowerCase() == name)
+    const courier = {
+      client: currentCourier.name,
+      id: currentCourier.id
+    }
+
+    yield put(actionObject(SET_SHIPMENT_VALUES, {
+      sizes,
+      courier,
+      destiny
+    }))
 
   } catch (error) {
 		yield put(actionObject(CREATE_RATE_ASYNC, { betterPrice: null }))
--- src/store/shipment/reducer.ts
+++ src/store/shipment/reducer.ts
@@ -1,5 +1,5 @@
 import { AnyAction } from 'redux'
-import { CREATE_SHIPMENT_ASYNC } from './action-types'
+import { CREATE_SHIPMENT_ASYNC, SET_SHIPMENT_VALUES } from './action-types'
 import { LOGOUT } from '../auth/action-types';
 
 const initialState = {
@@ -15,6 +15,16 @@ const reducer = (state = initialState, { type, payload }: AnyAction) => {
   switch(type) {
     case CREATE_SHIPMENT_ASYNC:
       return { ...state, ...payload }
+
+    case SET_SHIPMENT_VALUES:
+      return {
+        ...state,
+        form: {
+          sizes: {...payload.sizes},
+          courier: {...payload.courier},
+          destiny: {...payload.destiny}
+        }
+      }
 
     case LOGOUT:
       return initialState
