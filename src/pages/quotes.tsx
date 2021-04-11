d1875f6294e428256f5805e631256ce58dd726e5
Victor Caceres <caceres.vc95@gmail.com>
2021-04-11 11:32:44 -0400

    Add stepper in quotes page

--- src/pages/quotes.tsx
+++ src/pages/quotes.tsx
@@ -1,9 +1,37 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { HomeLayout } from '@components'
 import Head from 'next/head'
-import styles from '@styles/Home.module.scss'
+import styles from '@styles/Quotes.module.scss'
+
+import { Steps, Button, message } from 'antd';
+const { Step } = Steps;
+
+const steps = [
+  {
+    title: 'First',
+    content: 'First-content',
+  },
+  {
+    title: 'Second',
+    content: 'Second-content',
+  },
+  {
+    title: 'Last',
+    content: 'Last-content',
+  },
+];
 
 const Quotes: FC = () => {
+  const [current, setCurrent] = useState(0);
+
+  const next = () => {
+    setCurrent(current + 1);
+  };
+
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+
   return (
     <>
       <Head>
@@ -12,9 +40,31 @@ const Quotes: FC = () => {
       </Head>
 
       <HomeLayout>
-        <div style={{ height: '100vh' }}>
-          Quotes
-        </div>
+        <>
+          <Steps current={current}>
+            {steps.map(item => (
+              <Step key={item.title} title={item.title} />
+            ))}
+          </Steps>
+          <div className={styles.steps_content}>{steps[current].content}</div>
+          <div className={styles.steps_action}>
+            {current < steps.length - 1 && (
+              <Button type="primary" onClick={() => next()}>
+                Next
+              </Button>
+            )}
+            {current === steps.length - 1 && (
+              <Button type="primary" onClick={() => message.success('Processing complete!')}>
+                Done
+              </Button>
+            )}
+            {current > 0 && (
+              <Button style={{ margin: '0 8px' }} onClick={() => prev()}>
+                Previous
+              </Button>
+            )}
+          </div>
+        </>
       </HomeLayout>
     </>
   )
